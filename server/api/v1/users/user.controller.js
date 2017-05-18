1480e244dcccf402b0a910c7c6e43a733c65b3ca
Ali Mousa <amousa11@gmail.com>
2017-05-18 07:51:38 -0700

    balance converted to ether

--- server/api/v1/users/user.controller.js
+++ server/api/v1/users/user.controller.js
@@ -7,6 +7,8 @@ const path = require('path');
 const serverPath = './server';
 const dapp = require(`${path.join(process.cwd(), serverPath)}/dapp/dapp.js`)(config.contractsPath);
 const ProjectState = ba.rest.getEnums(`${config.libPath}/project/contracts/ProjectState.sol`).ProjectState;
+const BigNumber = common.BigNumber
+const constants = common.constants
 
 const usersController = {
   getBalance: function(req, res) {
@@ -18,7 +20,8 @@ const usersController = {
       .then(scope => {
         util.response.status200(res, {
           // this is a bignumber
-          balance: scope.result.toString()
+          balance: scope.result,
+          balanceBigNumber: new BigNumber(scope.result).div(constants.ETHER).toString()
         });
       })
       .catch(err => {
