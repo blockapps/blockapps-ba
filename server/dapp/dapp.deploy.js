94a94814e78732ece3087902dcea4fa1c4062f7f
nikitamendelbaum <nikitamendelbaum@gmail.com>
2018-06-05 14:12:50 -0400

    wait for account being fauceted on deploy

--- server/dapp/dapp.deploy.js
+++ server/dapp/dapp.deploy.js
@@ -27,6 +27,8 @@ describe('Supply Chain Demo App - deploy contracts', function () {
   it('should upload the contracts', function* () {
     // get the dapp
     const admin = yield rest.createUser(adminName, adminPassword);
+    // wait for the transaction to be added to blockchain
+    do yield new Promise(resolve => setTimeout(resolve, 1000)) while ((yield rest.getBalance(admin.address)) < 1);
     const dapp = yield dappJs.uploadContract(admin, config.libPath);
     const deployment = yield dapp.deploy(config.dataFilename, config.deployFilename);
   });
