eee76e94fd8ec4275e29aae4827d6754e0376fbb
Tim Holland <tim@blockapps.net>
2018-06-29 12:53:54 -0400

    Throw the error because logging isn't seen

--- server/lib/project/test/projectManager.test.js
+++ server/lib/project/test/projectManager.test.js
@@ -381,17 +381,20 @@ describe('ProjectManager Life Cycle tests', function() {
       account: admin.address,
     }
     let errorCode;
+    let err2;
     console.log("Attempting to accept a bid: " + JSON.stringify(buyer) + ", " + JSON.stringify(bid) + ", " + JSON.stringify(project));
     try {
       let x = yield contract.acceptBid(buyer, bid.id, project.name);
       console.log("acceptBid Succeeded: " + JSON.stringify(x));
     } catch(error) {
       console.log("We caught an error: " + JSON.stringify(error));
+      err2 = error;
       errorCode = parseInt(error.message);
     }
     console.log("The errorCode is: " + JSON.stringify(errorCode));
     // did not FAIL - that is an error
     assert.isDefined(errorCode, 'accepting a bid with insufficient balance should fail');
+    assert(!isNaN(errorCode), "Somebody gave us garbage: " + JSON.stringify(err2));
     // error should be INSUFFICIENT_BALANCE
     assert.equal(errorCode, ErrorCodes.INSUFFICIENT_BALANCE, 'error should be INSUFFICIENT_BALANCE.');
     // check that none was affected
