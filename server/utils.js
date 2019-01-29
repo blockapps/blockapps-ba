102db52c97925512509443bc07b447abf34d14e5
Tanuj Soni <tanuj@blockapps.net>
2019-01-29 14:55:18 +0530

    [private-chain] Fix: Bug fixes

--- server/utils.js
+++ server/utils.js
@@ -2,7 +2,7 @@ const ba = require('blockapps-rest');
 const rest = ba.rest6;
 const jwtDecode = require('jwt-decode');
 
-const createUser = function* (accessToken, emailId) {
+const createUser = function* (accessToken, userIdOptional = null) {
   let address = null;
   try {
     const getKeyResponse = yield rest.getKey(accessToken);
@@ -11,24 +11,27 @@ const createUser = function* (accessToken, emailId) {
     } else {
       return { status: 404, message: 'user address not found' };
     }
-  } catch (err) {
-    if (err.status == 400) {
+  } catch (getKeyErr) {
+    if (getKeyErr.status == 400) {
       try {
         const createKeyResponse = yield rest.createKey(accessToken);
         address = createKeyResponse.address;
-        yield rest.fill({ name: emailId, address: address });
-      } catch (err) {
-        return { status: err.status, message: 'error creating user key or faucet account' };
+        const userId = userIdOptional || getEmailIdFromToken(accessToken);
+        yield rest.fill({ name: userId, address });
+        if ((yield rest.getBalance(address)) < 1) {
+          do {
+            yield new Promise(resolve => setTimeout(resolve, 1000));
+          } while ((yield rest.getBalance(address)) < 1);
+        }
+      } catch (createKeyErr) {
+        return { status: createKeyErr.status, message: 'error creating user key or faucet account' };
       }
     } else {
-      return { status: err.status, message: 'error getting user key', error: err };
+      return { status: getKeyErr.status, message: `error getting user key; server returned error: ${getKeyErr}` };
     }
   }
-  do {
-    yield new Promise(resolve => setTimeout(resolve, 1000))
-  } while ((yield rest.getBalance(address)) < 1);
   return { status: 200, message: 'success', address: address };
-}
+};
 
 const getEmailIdFromToken = function(accessToken) {
   return (jwtDecode(accessToken)['email']);
